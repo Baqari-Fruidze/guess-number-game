180c94f92f93840e6781035123f506824c6d1b2b
Baqari-Fruidze <baqarifruidze@gmail.com>
2025-02-17 17:42:01 +0400

    possible fix

--- App.js
+++ App.js
@@ -25,3 +25,32 @@ const styles = StyleSheet.create({
   },
   backgroundImage: { opacity: 0.01 },
 });
+
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, Text, View, ImageBackground } from "react-native";
+// import StartGameScreen from "./screens/StartGameScreen";
+// import { LinearGradient } from "expo-linear-gradient";
+
+// export default function App() {
+//   return (
+//     <ImageBackground
+//       source={require("./assets/images/dice.jpg")} // Adjust the path
+//       resizeMode="cover"
+//       style={styles.rootScreen}
+//       imageStyle={styles.backgroundImage}
+//     >
+//       <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+//         <StartGameScreen />
+//       </LinearGradient>
+//     </ImageBackground>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   rootScreen: {
+//     flex: 1,
+//   },
+//   backgroundImage: {
+//     opacity: 0.2, // Adjust opacity to blend with gradient
+//   },
+// });
